b3732d3b18d6f025c98bdff46b753a62e644353a
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-03-31 19:08:45 +0200

    Start Footer & Heder, will change header into Nav in next update

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+
+    return (
+        <div className='Footer'>
+            <section>
+                <p>Köpa & sälja</p>
+            </section>
+            <section>
+                <p>Om Oss</p>
+            </section>
+            <section>
+                <p>Kontakta oss</p>
+            </section>
+        </div>
+    )
+}
+
+export default Footer;
