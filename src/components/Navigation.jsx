cfc4c1891403d0c159dd57fe3023bcb03fb331d3
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-04-04 11:16:49 +0200

    Header/navbar changes. search has ref and button links back to home

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,22 @@
+import { useRef } from "react";
 import { NavLink } from "react-router-dom";
 
+
 const Navigation = () => {
+    const input = useRef()
     return (
-        <nav> 
+        <div>
+            <h1>Auctonera Mera!</h1>
+        <nav className="NavBar"> 
             <NavLink to="/">Home</NavLink>
             <NavLink to="create">Create Auction</NavLink>
-            <input type="Search" placeholder="Search for any auction" />   
+            <input type="Search" ref={input} placeholder="Search for any auction" />
+            <NavLink to="/">
+            <button>Search</button>
+                </NavLink>
         </nav>
+        </div>
+
     )
 }
 
