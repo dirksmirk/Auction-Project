26731dde649f05be817f397d25b8f24375b6b4b3
Calandra Leslie <calandra.leslie@gmail.com>
2024-04-07 15:29:21 +0200

    connect home to bid?

--- src/components/SwitchPages/Home.jsx
+++ src/components/SwitchPages/Home.jsx
@@ -1,59 +1,5 @@
-/* import { useState, useEffect } from 'react'
-import BidAuction from './BidAuction'
-import AuctionItem from './smaller components/AuctionItem'
-
-function Home () {
-    const [Auctions, setAuctions] = useState(null)
-
-
-    useEffect(() => {
-        fetch(`https://auctioneer.azurewebsites.net/auction/7bac`)
-        .then((response) => response.json())
-        .then(result => {
-            console.log(result)
-            setAuctions(result)
-        })
-        return() => {
-            console.log(Auctions)
-        }
-    }, []) */
-
-    /* const getAuction = () => {
-        fetch(`https://auctioneer.azurewebsites.net/auction/7bac`)
-        .then((response) => response.json)
-        .then(result => {
-            console.log(result)
-            setAuctions(result)
-        })
-    } */
-
-    /* return(
-        <>
-            <div>
-                {Auctions && <ul>
-                    {Auctions.map((auction, idx) => (
-                        <li key={idx}> */
-                            {/* <p>{auction.AuctionID}</p>
-                            <p>{auction.Title}</p>
-                            <p>{auction.Description}</p>
-                            <p>{auction.StartingPrice}</p>
-                            <p>{auction.CreatedBy}</p>
-                            <p>{auction.StartDate}</p>
-                            <p>{auction.EndDate}</p> */}
-                            {/* <AuctionItem auction={auction}/>
-                        </li>
-                    ))}
-                </ul>}
-            </div>
-        </>
-    )
-}
-
-export default Home; */}
-
-/* I commented the previous code and used inline styling (I know we are not supposed to do that but needed the boxes to Fit ) I (hopefully) have started to make it so there are 5 auctions in a row and have added a button so it takes you to the bidding page (button is not active) I left the red around the boxes so they show useParams, but added a box around it so we can see how it looks unstyled. If you want we can test it by having more auctions on the page to see if it functions correctly (I am not really sure how to create an auction and do not want to mess with that code on that page)  */
-
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import AuctionItem from './smaller components/AuctionItem';
 
 function Home() {
@@ -62,7 +8,7 @@ function Home() {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('https://auctioneer.azurewebsites.net/auction/7bac')
+        fetch('https://auctioneer2.azurewebsites.net/auction/7bac')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
@@ -94,7 +40,9 @@ function Home() {
                 {auctions.length > 0 && auctions.slice(0, 5).map((auction, index) => (
                     <div key={index} style={{ border: '1px solid #ccc', padding: '20px', margin: '10px', textAlign: 'center', flex: '0 0 20%' }}>
                         <AuctionItem auction={auction} />
-                        <button style={{ marginTop: '10px' }}>Skapa Bud</button>
+                        <Link to={`/bid/${auction.AuctionID}`} style={{ textDecoration: 'none' }}> {/* Use Link instead of button */}
+                            <button style={{ marginTop: '10px' }}>Bid on this auction</button>
+                        </Link>
                     </div>
                 ))}
             </div>
