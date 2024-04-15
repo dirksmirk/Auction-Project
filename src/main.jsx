84aeba7409b74e0a14ac93fccdd525cf679d7574
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-04-15 10:51:46 +0200

    Chakra v2 installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </BrowserRouter>
 )
