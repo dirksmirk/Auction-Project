bc91c6348214d61518cb823204e9d95124d07cf7
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-04-16 22:13:43 +0200

    extendTheme implemented for colors

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './css/index.css'
 
+//extend color theme
+const colors = {
+
+}
+
+const theme = extendTheme({ colors })
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </BrowserRouter>
 )
+
