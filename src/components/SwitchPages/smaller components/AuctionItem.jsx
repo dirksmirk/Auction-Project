3936db998887e910102580b1064b194a55dae4e1
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-04-17 13:37:20 +0200

    css, home and auctionitem small changes

--- src/components/SwitchPages/smaller components/AuctionItem.jsx
+++ src/components/SwitchPages/smaller components/AuctionItem.jsx
@@ -1,4 +1,4 @@
-import { Heading, Text, Button, Card, CardBody } from '@chakra-ui/react'
+import { Heading, Text, Button, Card, CardBody, Center } from '@chakra-ui/react'
 
 const AuctionItem = ({ auction, isBidding, setAuctionId, onDelete }) => {
     
@@ -7,8 +7,13 @@ const AuctionItem = ({ auction, isBidding, setAuctionId, onDelete }) => {
         onDelete(auction.AuctionID);
     };
 
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toISOString().slice(0, 10);
+    };
+
     return (
-        <Card padding={5} /* style={{ border: '1px solid red', padding: '10px' }} */>
+        <Card padding={5} maxWidth='fit-content' alignSelf='center' /* style={{ border: '1px solid red', padding: '10px' }} */>
             {auction && (
                 <>
                     <Heading as='h1'>{auction.Title}</Heading>
@@ -17,8 +22,8 @@ const AuctionItem = ({ auction, isBidding, setAuctionId, onDelete }) => {
                             <Text><b>Description: </b> - {auction.Description}</Text>}
                         <Text><b>Start price</b> - {auction.StartingPrice}</Text>
                         <Text><b>Seller</b> - {auction.CreatedBy}</Text>
-                        <Text><b>Start date</b> -  <br />{auction.StartDate}</Text>
-                        <Text><b>End date</b> - <br />{auction.EndDate}</Text>
+                        <Text><b>Start date</b> -  {formatDate(auction.StartDate)}</Text>
+                        <Text><b>End date</b> - {formatDate(auction.EndDate)}</Text>
                         {onDelete && 
                             <Button 
                             _hover={{ boxShadow: 'xl', background: 'red', color: 'white' }}
