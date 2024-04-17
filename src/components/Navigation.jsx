64bc974fc6e309cdf38079bf2d82d403ca693a63
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-04-17 14:15:06 +0200

    reset fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,14 @@
-import React, { useContext, useRef } from 'react';
+import { useContext, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { SearchContext } from '../Context';
 import {
   Box,
   Flex,
-  IconButton,
   Input,
   Button,
-  useDisclosure,
   useColorModeValue,
   HStack,
 } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
 const Links = [
   { to: '/', text: 'Home' },
@@ -21,7 +18,6 @@ const Links = [
 const Navigation = () => {
   const { myUpdateFunc } = useContext(SearchContext);
   const input = useRef();
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleSearch = () => {
     const inputValue = input.current.value.trim(); // Remove leading and trailing whitespaces
@@ -35,21 +31,19 @@ const Navigation = () => {
       console.log(inputValue);
     }
   };
+  // reset function for home and create auction inputs, so that search resets
+  const Reset = () => {
+    input.current.value = "";
+    myUpdateFunc("");
+  };
 
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
       <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-        <IconButton
-          size={'md'}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={'Open Menu'}
-          display={{ md: 'none' }}
-          onClick={isOpen ? onClose : onOpen}
-        />
         <HStack spacing={8} alignItems={'center'}>
           <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
             {Links.map((link) => (
-              <NavLink key={link.to} to={link.to}>
+              <NavLink key={link.to} to={link.to} onClick={Reset}>
                 {link.text}
               </NavLink>
             ))}
@@ -59,7 +53,10 @@ const Navigation = () => {
           <Input
             ref={input}
             placeholder="Search"
-            variant="unstyled"
+            border='2px' 
+            borderStyle='ridge' 
+            borderColor='gray' 
+            borderRadius={5}
             size="sm"
             pr="4.5rem"
           />
@@ -68,22 +65,10 @@ const Navigation = () => {
             colorScheme="blue"
             onClick={handleSearch}
             ml={2}
-          >
-            Search
+          >Search
           </Button>
         </Flex>
       </Flex>
-      {isOpen ? (
-        <Box pb={4} display={{ md: 'none' }}>
-          <HStack as={'nav'} spacing={4}>
-            {Links.map((link) => (
-              <NavLink key={link.to} to={link.to}>
-                {link.text}
-              </NavLink>
-            ))}
-          </HStack>
-        </Box>
-      ) : null}
     </Box>
   );
 };
